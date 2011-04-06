db1881f543f27136c357bcd26fbd065c2f1ac308
Jasje <bonkebesje@gmail.com>
2011-04-06 16:18:22 +0200

    Updated tukz script for shaman weapon enchants

--- Tukui/modules/notifications/selfbuffs.lua
+++ Tukui/modules/notifications/selfbuffs.lua
@@ -49,6 +49,7 @@ T.remindenchants = {
 	},
 	SHAMAN = {
 		8024, -- flametongue
+		8232, -- windfury
 		51730, -- earthliving
 	},
 }
@@ -107,7 +108,11 @@ if (buffs and buffs[1]) then
 
 	frame.icon = frame:CreateTexture(nil, "OVERLAY")
 	frame.icon:SetPoint("CENTER")
+if class == "SHAMAN" then
+	frame:CreatePanel("Default", 40, 40, "CENTER", UIParent, "CENTER", -30, 200)
+else
 	frame:CreatePanel("Default", 40, 40, "CENTER", UIParent, "CENTER", 0, 200)
+end
 	frame.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
 	frame.icon:Size(36)
 	frame:Hide()
@@ -126,15 +131,20 @@ end
 
 if (enchants and enchants[1]) then
 	local sound
+	local currentlevel = UnitLevel("player")
+
 	local function EnchantsOnEvent(self, event)
-		if (event == "PLAYER_LOGIN") or (event == "ACTIVE_TALENT_GROUP_CHANGED") then
+		if (event == "PLAYER_LOGIN") or (event == "ACTIVE_TALENT_GROUP_CHANGED") or (event == "PLAYER_LEVEL_UP") then
 			if class == "ROGUE" then
 				self:UnregisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
+				self:UnregisterEvent("PLAYER_LEVEL_UP")
 				self.icon:SetTexture(select(3, GetSpellInfo(enchants[1])))
 				return
 			elseif class == "SHAMAN" then
 				local ptt = GetPrimaryTalentTree()
 				if ptt and ptt == 3 and currentlevel > 53 then
+					self.icon:SetTexture(select(3, GetSpellInfo(enchants[3])))
+				elseif ptt and ptt == 2 and currentlevel > 31 then
 					self.icon:SetTexture(select(3, GetSpellInfo(enchants[2])))
 				else
 					self.icon:SetTexture(select(3, GetSpellInfo(enchants[1])))
@@ -143,14 +153,13 @@ if (enchants and enchants[1]) then
 			end
 		end
 
-		local currentlevel = UnitLevel("player")
 		if (class == "ROGUE" or class =="SHAMAN") and currentlevel < 10 then return end
 
 		if (UnitAffectingCombat("player") and not UnitInVehicle("player")) then
 			local mainhand, _, _, offhand, _, _, thrown = GetWeaponEnchantInfo()
 			if class == "ROGUE" then
 				local itemid = GetInventoryItemID("player", GetInventorySlotInfo("RangedSlot"))
-				if itemid and select(7, GetItemInfo(itemid)) == "Thrown" and currentlevel > 61 then
+				if itemid and select(7, GetItemInfo(itemid)) == INVTYPE_THROWN and currentlevel > 61 then
 					if mainhand and offhand and thrown then
 						self:Hide()
 						sound = true
@@ -164,7 +173,14 @@ if (enchants and enchants[1]) then
 					end
 				end
 			elseif class == "SHAMAN" then
-				if mainhand then
+				local itemid = GetInventoryItemID("player", GetInventorySlotInfo("SecondaryHandSlot"))
+				if itemid and select(6, GetItemInfo(itemid)) == ENCHSLOT_WEAPON then
+					if mainhand and offhand then
+						self:Hide()
+						sound = true
+						return
+					end
+				elseif mainhand then
 					self:Hide()
 					sound = true
 					return
@@ -185,12 +201,17 @@ if (enchants and enchants[1]) then
 
 	frame.icon = frame:CreateTexture(nil, "OVERLAY")
 	frame.icon:SetPoint("CENTER")
+if class == "SHAMAN" then
+	frame:CreatePanel("Default", 40, 40, "CENTER", UIParent, "CENTER", 30, 200)
+else
 	frame:CreatePanel("Default", 40, 40, "CENTER", UIParent, "CENTER", 0, 200)
+end	
 	frame.icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
 	frame.icon:Size(36)
 	frame:Hide()
 
 	frame:RegisterEvent("PLAYER_LOGIN")
+	frame:RegisterEvent("PLAYER_LEVEL_UP")
 	frame:RegisterEvent("PLAYER_REGEN_ENABLED")
 	frame:RegisterEvent("PLAYER_REGEN_DISABLED")
 	frame:RegisterEvent("UNIT_INVENTORY_CHANGED")
