5c9b40e1d5f8df638bfd835ec8ac05f0cd610862
Jasje <bonkebesje@gmail.com>
2011-04-28 16:29:26 +0200

    Extra with interupt announce, fdispelannounce, allertrun, saftExperienceBar and XanAutomail

--- Tukui/modules/extra/extra.lua
+++ Tukui/modules/extra/extra.lua
@@ -168,6 +168,74 @@ local a = CreateFrame ("Frame")
 		end
 	end)
 
+-----------------------------------------
+-- fDispelAnnounce made by Foof
+-----------------------------------------
+
+local fDispelAnnounce = CreateFrame("Frame", fDispelAnnounce)
+local band = bit.band
+local font = C.media.pixelfont -- HOOG0555.ttf 
+local fontflag = "OUTLINEMONOCHROME" -- for pixelfont stick to this else OUTLINE or THINOUTLINE
+local fontsize = 18 -- font size
+local COMBATLOG_OBJECT_AFFILIATION_MINE = COMBATLOG_OBJECT_AFFILIATION_MINE
+ 
+-- Registered events
+local events = {
+	["SPELL_STOLEN"] = {
+		["enabled"] = true,
+		["msg"] = "Removed",
+		["color"] = "69CCF0",
+	},
+	["SPELL_DISPEL"] = {
+		["enabled"] = true,
+		["msg"] = "Removed",
+		["color"] = "3BFF33",
+	},
+	["SPELL_DISPEL_FAILED"] = {
+		["enabled"] = true,
+		["msg"] = "FAILED",
+		["color"] = "C41F3B",
+	},
+	["SPELL_HEAL"] = {
+		["enabled"] = false,
+		["msg"] = "Healed",
+		["color"] = "3BFF33",
+	},
+}
+ 
+-- Frame function
+local function CreateMessageFrame(name)
+	local f = CreateFrame("ScrollingMessageFrame", name, UIParent)
+	f:SetHeight(80)
+	f:SetWidth(500)
+	f:SetPoint("CENTER", 0, 150)
+	f:SetFrameStrata("HIGH")
+	f:SetTimeVisible(1.5)
+	f:SetFadeDuration(3)
+	f:SetMaxLines(3)
+	f:SetFont(font, fontsize, fontflag)
+	return f
+end
+ 
+-- Create messageframe
+local dispelMessages = CreateMessageFrame("fDispelFrame")
+ 
+local function OnEvent(self, event, timestamp, eventType, hideCaster, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags, ...)
+	if(not events[eventType] or not events[eventType].enabled or band(sourceFlags, COMBATLOG_OBJECT_AFFILIATION_MINE) ~= COMBATLOG_OBJECT_AFFILIATION_MINE or sourceGUID ~= UnitGUID("player")) then
+		return
+	end
+	-- Print to partychat
+	local numraid = GetNumRaidMembers()
+	if (numraid > 0 and numraid < 6) then
+		SendChatMessage(events[eventType].msg .. ": " .. select(5, ...), "PARTY")
+	end
+	-- Add to messageframe
+	dispelMessages:AddMessage("|cff" .. events[eventType].color .. events[eventType].msg .. ":|r " .. select(5, ...))
+end
+ 
+-- finally
+fDispelAnnounce:RegisterEvent('COMBAT_LOG_EVENT_UNFILTERED')
+fDispelAnnounce:SetScript('OnEvent', OnEvent)
 
 ----------------------------------------------------------------------------------------------------------------
 -- saftExperienceBar by Safturento
