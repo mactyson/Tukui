381407c6964220281358cf5a044349ccf993ce11
Jasje <bonkebesje@gmail.com>
2011-05-05 16:31:33 +0200

    Shows you Average item level your target is currently wearing in the tooltip. http://www.tukui.org/forums/topic.php?id=10151

--- Tukui/modules/tooltip/tooltip.lua
+++ Tukui/modules/tooltip/tooltip.lua
@@ -398,3 +398,36 @@ TukuiTooltip:SetScript("OnEvent", function(self)
 		hooksecurefunc(GameTooltip, "SetShapeshift", CombatHideActionButtonsTooltip)
 	end
 end)
+-- ilvl on tooltip by Gsuz
+local SlotName = {
+        "Head","Neck","Shoulder","Back","Chest","Wrist",
+        "Hands","Waist","Legs","Feet","Finger0","Finger1",
+        "Trinket0","Trinket1","MainHand","SecondaryHand","Ranged","Ammo"
+    }
+local function GetItemLVL(unit)
+local total, item = 0, 0;
+    for i in pairs(SlotName) do
+        local slot = GetInventoryItemLink(unit, GetInventorySlotInfo(SlotName[i] .. "Slot"));
+        if (slot ~= nil) then
+            item = item + 1;
+            total = total + select(4, GetItemInfo(slot))
+        end
+    end
+    if (item > 0) then
+        return floor(total / item);
+    end
+    return 0;
+end
+GameTooltip:HookScript("OnTooltipSetUnit", function(self, ...)
+    if (IsShiftKeyDown()) then
+        local _, unit = GameTooltip:GetUnit();
+        if (unit and CanInspect(unit)) then
+            if (not ((InspectFrame and InspectFrame:IsShown()) or (Examiner and Examiner:IsShown()))) then
+                NotifyInspect(unit);
+                GameTooltip:AddLine(hexa.."Item Level: "..hexb.. GetItemLVL(unit));
+                ClearInspectPlayer(unit);
+                GameTooltip:Show();
+            end
+        end
+    end
+end)
