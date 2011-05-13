18c47d233bf3798824a4bd19f1937c3d727c58f4
Jasje <bonkebesje@gmail.com>
2011-05-13 15:34:05 +0200

    Move some code for KeyBindUI.

--- Tukui/modules/skins/blizzardframes.lua
+++ Tukui/modules/skins/blizzardframes.lua
@@ -198,6 +198,41 @@ TukuiSkin:RegisterEvent("ADDON_LOADED")
 TukuiSkin:SetScript("OnEvent", function(self, event, addon)
 	if IsAddOnLoaded("Skinner") or IsAddOnLoaded("Aurora") then return end
 
+	--Keybinds
+	if addon == "Blizzard_BindingUI" then
+		local buttons = {
+			"KeyBindingFrameDefaultButton",
+			"KeyBindingFrameUnbindButton",
+			"KeyBindingFrameOkayButton",
+			"KeyBindingFrameCancelButton",
+		}
+
+		for _, v in pairs(buttons) do
+			_G[v]:StripTextures()
+			_G[v]:SetTemplate("Default", true)
+		end
+
+		SkinCheckBox(KeyBindingFrameCharacterButton)
+		KeyBindingFrameHeaderText:ClearAllPoints()
+		KeyBindingFrameHeaderText:Point("TOP", KeyBindingFrame, "TOP", 0, -4)
+		KeyBindingFrame:StripTextures()
+		KeyBindingFrame:SetTemplate("Transparent")
+
+		for i = 1, KEY_BINDINGS_DISPLAYED  do
+			local button1 = _G["KeyBindingFrameBinding"..i.."Key1Button"]
+			local button2 = _G["KeyBindingFrameBinding"..i.."Key2Button"]
+			button1:StripTextures(true)
+			button1:StyleButton(false)
+			button1:SetTemplate("Default", true)
+			button2:StripTextures(true)
+			button2:StyleButton(false)
+			button2:SetTemplate("Default", true)
+		end
+
+		KeyBindingFrameUnbindButton:Point("RIGHT", KeyBindingFrameOkayButton, "LEFT", -3, 0)
+		KeyBindingFrameOkayButton:Point("RIGHT", KeyBindingFrameCancelButton, "LEFT", -3, 0)
+	end	
+	
     --GBANK
 	if addon == "Blizzard_GuildBankUI" then
 		GuildBankFrame:StripTextures()
@@ -1315,38 +1350,6 @@ TukuiSkin:SetScript("OnEvent", function(self, event, addon)
 		ItemSocketingSocketButton:Point("BOTTOMRIGHT", ItemSocketingFrame, "BOTTOMRIGHT", -5, 5)
 		SkinButton(ItemSocketingSocketButton)
 		SkinCloseButton(ItemSocketingCloseButton)
-	end
-	
-	-- Keybinding frame
-	if addon == "Blizzard_BindingUI" then
-		local buttons = {
-			"KeyBindingFrameDefaultButton",
-			"KeyBindingFrameUnbindButton",
-			"KeyBindingFrameOkayButton",
-			"KeyBindingFrameCancelButton",
-		}
-		
-		for _, v in pairs(buttons) do
-			_G[v]:StripTextures()
-			_G[v]:SetTemplate("Default")
-		end
-		
-		SkinCheckBox(KeyBindingFrameCharacterButton)
-		KeyBindingFrameHeaderText:ClearAllPoints()
-		KeyBindingFrameHeaderText:Point("TOP", KeyBindingFrame, "TOP", 0, -4)
-		KeyBindingFrame:StripTextures()
-		KeyBindingFrame:SetTemplate("Transparent")
-		
-		for i = 1, KEY_BINDINGS_DISPLAYED  do
-			local button1 = _G["KeyBindingFrameBinding"..i.."Key1Button"]
-			local button2 = _G["KeyBindingFrameBinding"..i.."Key2Button"]
-			button1:StripTextures(true)
-			button1:StyleButton(false)
-			button1:SetTemplate("Default", true)
-			button2:StripTextures(true)
-			button2:StyleButton(false)
-			button2:SetTemplate("Default", true)
-		end
 	end
 	
 	if addon == "Blizzard_GlyphUI" then
